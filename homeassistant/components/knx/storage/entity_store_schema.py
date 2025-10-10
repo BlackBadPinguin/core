7f66b9de90f8918212c5affc73124750fa82bf09
farmio <farmio@alphart.net>
2025-10-10 15:52:08 +0200

    Remove unused Enum, use Enum values instead of hardcode str

--- homeassistant/components/knx/storage/entity_store_schema.py
+++ homeassistant/components/knx/storage/entity_store_schema.py
@@ -354,15 +354,7 @@ class ConfSetpointShiftMode(StrEnum):
 
 
 @unique
-class ActiveMode(StrEnum):
-    """Enum for active mode."""
-
-    BINARY = "1"
-    VALVE = "5.001"
-
-
-@unique
-class ClimateFanSpeedMode(StrEnum):
+class ConfClimateFanSpeedMode(StrEnum):
     """Enum for climate fan speed mode."""
 
     PERCENTAGE = "5.001"
@@ -473,7 +465,7 @@ CLIMATE_KNX_SCHEMA = vol.Schema(
             )
         ),
         "section_fan": KNXSectionFlat(collapsible=True),
-        vol.Optional(CONF_GA_FAN_SPEED): GASelector(dpt=ClimateFanSpeedMode),
+        vol.Optional(CONF_GA_FAN_SPEED): GASelector(dpt=ConfClimateFanSpeedMode),
         vol.Required(ClimateConf.FAN_MAX_STEP, default=3): AllSerializeFirst(
             selector.NumberSelector(
                 selector.NumberSelectorConfig(min=1, max=100, step=1)
